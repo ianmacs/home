0b6b519adfe0752780f7919fb40cb68fc4141d9f
ianmacs <ianmacs@example.com>
2021-04-10 14:37:43 +0200

    Make setup script non-interactive

--- setup_my_project.sh
+++ setup_my_project.sh
@@ -1,41 +1,45 @@
-#!/bin/bash
+#!/bin/bash -ex
+
+# This script will detach this repository from its origin,
+# set the username and password for MQTT in your project,
+# set the database name for influxdb and delete itself.
+
+# Change the following settings for your project:
+
+# set the mosquitto username and password, separated by colon.
+MQTT_USER_PASSWORD=home:password
+
+# set the influxdb database name
+INFLUXDB_DATABASE=home
+
+# No more settings below this line.
 
 cd $(dirname $0)
 
+# Do not execute setup a second time
+if test -f setup
+then echo "setup has already been run. exiting."
+     exit 1
+fi
+
+# Check if docker works before changing anything
+docker pull eclipse-mosquitto
+
+# Check if git works before starting for real
 date >setup
 git add setup
 git commit -m 'setup' setup || exit 1
 
-function confirm ()
-{
-    read C
-    echo -- "$C" | grep -qi '^y'
-}
-
-function confirm_final()
-{
-    if confirm
-    then return
-    else exit 1
-    fi
-}
-
-echo "To start a home automation project from this repository, you should remove"
-echo "the original git origin repository to avoid accidental pushes."
-echo -n "Remove git origin? (y/n) "
-if confirm
+if true
 then git remote origin
-     echo "Origin removed."
-else echo "Not removing origin."
+     echo "Git origin removed."
+else echo "Not removing git origin."
 fi
 
-docker pull eclipse-mosquitto
-echo
-echo "Set username and password for mosquitto"
-echo -n "Enter user name:"
-read MQTT_USER
-echo Setting password for user "$MQTT_USER"
-docker run --rm -i eclipse-mosquitto mosquitto_passwd -c /proc/self/fd/2 "$MQTT_USER" 2>mosquitto/config/passwordfile
-git commit -m "Set MQTT user name and password" mosquitto/config/passwordfile
+echo "Setting username and password for mosquitto"
+echo "$MQTT_USER_PASSWORD" >mosquitto/config/passwordfile
+docker run --rm -i -v$PWD/mosquitto/config/passwordfile:/passwordfile eclipse-mosquitto mosquitto_passwd -U /passwordfile
+
+
 
 docker-compose up -d
